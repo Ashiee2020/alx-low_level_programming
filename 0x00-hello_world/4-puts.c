a04e47c812eb27fa545f95b03ad4aeaf047e6da9
Ashiee2020 <mainaashley73@gmail.com>
2023-04-24 13:06:56 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return o
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main (void)
+{puts(""Programming is like building a multilingual puzzle\n");
+	return (0);
 }
+
+
+
+
