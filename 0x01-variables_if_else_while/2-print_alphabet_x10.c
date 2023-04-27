1fa44dd4718ae065403060158d24e513e87e351c
Ashiee2020 <mainaashley73@gmail.com>
2023-04-27 01:56:31 -0700

    ./2-alphabet_x10

---
+++ 0x01-variables_if_else_while/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	vhar c;
+	for (c = 'a'; c <= 'z'; c++)
+	purchar(c);
+	putchar('\n');
+	return (0);
+}
+
